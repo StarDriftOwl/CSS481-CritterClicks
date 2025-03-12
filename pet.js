fde1a180261f60a82f94814e7d92017baf035ba1
charlie6543 <sli34@uw.edu>
2025-03-11 20:47:03 -0700

    adding html/css for stats/levels/bag tab

--- pet.js
+++ pet.js
@@ -12,7 +12,10 @@ document.getElementById("next").onclick = () => {
         namePicker.classList.add("hide");
         document.querySelector("label").innerText = namePicker.value;
         document.getElementById("next").classList.add("hide");
+        document.querySelector("#bottomBar").classList.remove("hide");
+        document.querySelector("#shopTab").classList.remove("hide");
         pet.onclick = addCoin;
+
     }
 
 }
@@ -28,4 +31,29 @@ function choosePet(pet){
     let display = document.getElementById("pet");
     let petImg = pet.getAttribute("src");
     display.setAttribute("src", petImg);
+}
+
+function changeTabs(event, tabName){
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i = 0; i < tablinks.length; i++){
+        if(tablinks[i].innerText == tabName){
+            tablinks[i].classList.add("activeTabLink");
+        }
+        else{
+            tablinks[i].classList.remove("activeTabLink");
+
+        }
+        tablinks[i].className = tablinks[i].className.replace("  active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    
+    event.currentTarget.className += "  active";
 }
