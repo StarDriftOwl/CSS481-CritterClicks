27551f92eba7bcd54103bf9d062b32602d5aaefd
Star W ("Strawbunny") <starwong@uw.edu>
2025-03-18 00:29:31 -0700

    new

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// src/App.tsx
+import React from 'react';
+import Game from './game';
+// import './App.css';
+
+function App() {
+  console.log("App component rendering"); // Add this log
+  return (
+    // <div className="App" style={{ backgroundColor: "#f0f0f0", minHeight: "100vh" }}>
+     <div> 
+      <header>
+      </header>
+      <main>
+      <Game />
+      </main>
+    </div>
+  );
+}
+
+export default App;
